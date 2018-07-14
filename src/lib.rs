bc2ebdfffdab0b746aebfb01162040271c90da84
Daniel Stahl <danstahl1138@gmail.com>
2018-07-14 17:37:27 -0500

    added implied volatility

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
 //! # black_scholes
 //! A Black Scholes option pricing library.
 extern crate special;
+extern crate nrfind;
 use std::f64::consts::PI;
 use std::f64::consts::SQRT_2;
 use special::Error;
+#[macro_use]
+#[cfg(test)]
+extern crate approx;
+
 fn cum_norm(x:f64)->f64 {
     (x/SQRT_2).erf()*0.5+0.5
 }
@@ -138,6 +143,21 @@ pub fn put_theta(s:f64, k:f64, rate:f64, sigma:f64, maturity:f64)->f64{
     }
 }
 
+pub fn call_iv(price:f64, s:f64, k:f64, rate:f64, maturity:f64, initial_guess:f64)->f64{
+    let obj_fn=|sigma|call(s, k, rate, sigma, maturity)-price;
+    let dfn=|sigma|call_vega(s, k, rate, sigma, maturity);
+    let precision=0.00000001;
+    let iterations=20;
+    nrfind::find_root(&obj_fn, &dfn, initial_guess, precision, iterations).unwrap()
+}
+pub fn put_iv(price:f64, s:f64, k:f64, rate:f64, maturity:f64, initial_guess:f64)->f64{
+    let obj_fn=|sigma|put(s, k, rate, sigma, maturity)-price;
+    let dfn=|sigma|put_vega(s, k, rate, sigma, maturity);
+    let precision=0.00000001;
+    let iterations=20;
+    nrfind::find_root(&obj_fn, &dfn, initial_guess, precision, iterations).unwrap()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -156,5 +176,27 @@ mod tests {
     #[test]
     fn put_formula_works_with_zero_vol() {
         assert_eq!(put(5.0, 4.5, 0.05, 0.3, 0.0), 0.0);
+    }
+    #[test]
+    fn call_iv_works(){
+        let sigma=0.2;
+        let initial_guess=0.5;
+        let s=5.0;
+        let k=4.5;
+        let rate=0.05;
+        let maturity=1.0;
+        let price=call(s, k, rate, sigma, maturity);
+        assert_abs_diff_eq!(call_iv(price, s, k, rate, maturity , initial_guess), sigma, epsilon=0.00000001);
+    }
+    #[test]
+    fn put_iv_works(){
+        let sigma=0.2;
+        let initial_guess=0.5;
+        let s=5.0;
+        let k=4.5;
+        let rate=0.05;
+        let maturity=1.0;
+        let price=put(s, k, rate, sigma, maturity);
+        assert_abs_diff_eq!(put_iv(price, s, k, rate, maturity , initial_guess), sigma, epsilon=0.00000001);
     }
 }
