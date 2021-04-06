767e4ab625167d1b11ef7ca3b3d55829541d5b71
github-action-benchmark <github@users.noreply.github.com>
2021-04-06 01:27:39 +0000

    add Benchmark (cargo) benchmark result for 18945203756727dde0ccb6f17cba4ec1aae7de6c

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1617672442571,
+  "lastUpdate": 1617672459121,
   "repoUrl": "https://github.com/danielhstahl/black_scholes_rust",
   "entries": {
     "Benchmark": [
@@ -25,6 +25,36 @@ window.BENCHMARK_DATA = {
             "name": "bench_call_price",
             "value": 13,
             "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "danstahl1138@gmail.com",
+            "name": "Daniel Stahl",
+            "username": "danielhstahl"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "18945203756727dde0ccb6f17cba4ec1aae7de6c",
+          "message": "Merge pull request #7 from danielhstahl/feature/add_bench\n\nFeature/add bench",
+          "timestamp": "2021-04-05T20:26:10-05:00",
+          "tree_id": "4523d58804a8e05b7b6ef697d8871e6d16081cfd",
+          "url": "https://github.com/danielhstahl/black_scholes_rust/commit/18945203756727dde0ccb6f17cba4ec1aae7de6c"
+        },
+        "date": 1617672457593,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_call_price",
+            "value": 12,
+            "range": "± 2",
             "unit": "ns/iter"
           }
         ]
