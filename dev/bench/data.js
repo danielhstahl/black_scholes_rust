7cdbd2989f8e3060e374e2e6e5c2ea93e3f94c73
github-action-benchmark <github@users.noreply.github.com>
2021-04-06 01:27:22 +0000

    add Benchmark (cargo) benchmark result for 0ecc041abbd31950869cc19c14310afcac624d9f

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1617672442571,
+  "repoUrl": "https://github.com/danielhstahl/black_scholes_rust",
+  "entries": {
+    "Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "danielhstahl",
+            "username": "danielhstahl"
+          },
+          "committer": {
+            "name": "danielhstahl",
+            "username": "danielhstahl"
+          },
+          "id": "0ecc041abbd31950869cc19c14310afcac624d9f",
+          "message": "Feature/add bench",
+          "timestamp": "2021-04-04T14:15:20Z",
+          "url": "https://github.com/danielhstahl/black_scholes_rust/pull/7/commits/0ecc041abbd31950869cc19c14310afcac624d9f"
+        },
+        "date": 1617672441943,
+        "tool": "cargo",
+        "benches": [
+          {
+            "name": "bench_call_price",
+            "value": 13,
+            "range": "± 1",
+            "unit": "ns/iter"
+          }
+        ]
+      }
+    ]
+  }
+}
