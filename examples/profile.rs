e69debd798af37c95ac5512b04210fdf86d8f796
Markus Mayer <widemeadows@gmail.com>
2024-05-26 14:22:38 +0200

    Refactor normal distribution for f64 constants

--- examples/profile.rs
+++ examples/profile.rs
@@ -1,38 +1,16 @@
-#![feature(test)]
-extern crate test;
-use test::{black_box, Bencher};
+use std::hint::black_box;
 
 /// Convenience alias for [`black_box`].
 const BB: fn(f64) -> f64 = black_box;
 
-#[bench]
-fn bench_call_price(b: &mut Bencher) {
+fn main() {
     let r = 0.05;
     let sig = 0.3;
     let t = 1.0;
     let asset = 50.0;
     let k = 50.0;
-    b.iter(|| black_scholes::call(BB(asset), BB(k), BB(r), BB(sig), BB(t)))
-}
 
-#[bench]
-fn bench_all_price(b: &mut Bencher) {
-    let r = 0.05;
-    let sig = 0.3;
-    let t = 1.0;
-    let asset = 50.0;
-    let k = 50.0;
-    b.iter(|| black_scholes::compute_all(BB(asset), BB(k), BB(r), BB(sig), BB(t)))
-}
-
-#[bench]
-fn bench_all_price_no_cache(b: &mut Bencher) {
-    let r = 0.05;
-    let sig = 0.3;
-    let t = 1.0;
-    let asset = 50.0;
-    let k = 50.0;
-    b.iter(|| {
+    for _ in 0..100_000 {
         black_scholes::call(BB(asset), BB(k), BB(r), BB(sig), BB(t));
         black_scholes::call_delta(BB(asset), BB(k), BB(r), BB(sig), BB(t));
         black_scholes::call_gamma(BB(asset), BB(k), BB(r), BB(sig), BB(t));
@@ -46,5 +24,5 @@ fn bench_all_price_no_cache(b: &mut Bencher) {
         black_scholes::put_vega(BB(asset), BB(k), BB(r), BB(sig), BB(t));
         black_scholes::put_theta(BB(asset), BB(k), BB(r), BB(sig), BB(t));
         black_scholes::put_rho(BB(asset), BB(k), BB(r), BB(sig), BB(t));
-    })
+    }
 }
